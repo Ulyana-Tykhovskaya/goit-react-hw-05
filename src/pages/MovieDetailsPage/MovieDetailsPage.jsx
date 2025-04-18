c3ae5d2df15578e88427bd4b9e885ae9f994178e
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-04-18 13:29:11 +0200

    update file

---
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+const MovieDetailsPage = () => {
+  return (
+    <main>
+      <h1>Movie Details Page</h1>.
+      <Outlet />
+      {/* Здесь мы вложим маршруты что бы правильно рендерить он помогает если в одной странице есть еще несколько для дочерних элементов */}
+    </main>
+  );
+};
+export default MovieDetailsPage;
---
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,8 @@
+const MoviesPage = () => {
+  return (
+    <main>
+      <h1>Movies Page</h1>
+    </main>
+  );
+};
+export default MoviesPage;
